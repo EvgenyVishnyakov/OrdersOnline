5e819b890cd3d79a76670b5637ab9051a3c98165
Evgeny Vishnyakov <vis-evgenij@yandex.ru>
2025-05-29 23:00:44 +0300

    убрал лишний код

--- OnlineOrderDb/Models/ProductConfig.cs
+++ OnlineOrderDb/Models/ProductConfig.cs
@@ -14,23 +14,6 @@ namespace OnlineOrder.Db.Models
 
             entityTypeBuilder.HasKey(e => e.Id);
 
-            //entityTypeBuilder.HasData(
-            //        new ProductBuilder()
-            //    .WithId(firstProduct)
-            //    .Build(),
-
-            //        new ProductBuilder()
-            //    .WithId(secondProduct)
-            //    .Build(),
-
-            //        new ProductBuilder()
-            //    .WithId(thirdProduct)
-            //    .Build(),
-
-            //        new ProductBuilder()
-            //    .WithId(fourthProduct)
-            //    .Build()
-            //    );
         }
     }
 }
